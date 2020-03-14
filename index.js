93fb4bb7b242f67da3309afc88581f1085cc3735
Kenyon-j <Deathwinzzz@gmail.com>
2020-03-14 17:59:38 +0000

    points to html

--- index.js
+++ index.js
@@ -2,15 +2,19 @@
 
 const Koa = require('koa')
 const Router = require('koa-router')
+const staticDir = require('koa-static')
+const bodyParser = require('koa-bodyparser')
+const session = require('koa-session')
+
 const app = new Koa()
 const router = new Router()
 
-const defaultPort = 3000
-const port = process.env.PORT || defaultPort
+app.use(staticDir('public'))
+app.use(bodyParser())
+app.use(session(app))
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-  });
+const defaultPort = 8080
+const port = process.env.PORT || defaultPort
 
 app.use(router.routes())
 module.exports = app.listen(port, async() => console.log(`listening on port ${port}`))
